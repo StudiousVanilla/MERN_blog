bc570af2ca5cfa6ef0b05338889b99f6ab475eeb
Oisín_windows <byrne.ois@gmail.com>
2021-02-12 10:27:22 +0000

    initialising previously unsaved changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const {requireAuth} = require('../middleware/authMiddleware')
 
 // controllers
 const userController = require('../controllers/userController')
 
 // Get Blog and specific blog posts
-router.get('/', userController.placeHolderFunction)
-router.get('/:id', userController.placeHolderFunction)
+router.get('/', requireAuth, userController.placeHolderFunction)
+router.get('/:id', requireAuth, userController.placeHolderFunction)
 
 // Add a comment to a blog
-router.post('/:id', userController.placeHolderFunction)
+router.post('/:id', requireAuth, userController.placeHolderFunction)
 
 module.exports = router
