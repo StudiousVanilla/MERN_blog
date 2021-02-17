39338ab4dc4c3beb423771dbc950e063832be4a6
Oisín_windows <byrne.ois@gmail.com>
2021-02-17 11:03:13 +0000

    set cors origin to local host

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 // CORS settings
 const corsOptions = {
-  origin: 'http://localhost:3001/',
+  origin: 'http://localhost:3001',
   optionsSuccessStatus: 200,
   credentials: true
 }
